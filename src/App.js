29ff25991a4d6a210f5030a1dcd6f6038cfc035e
unknown <sai.kishore2015@gmail.com>
2020-02-27 22:01:33 +0530

    scss structure and added material design package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link hello"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
